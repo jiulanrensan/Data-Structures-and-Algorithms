7b276ce0a761c039aa957ff149e50d0cb58593e4
20190225001 <839492148@qq.com>
2021-08-07 21:09:56 +0800

    feat: 25

--- leetcode/25.js
+++ leetcode/25.js
@@ -12,5 +12,55 @@
  * @param {number} k
  * @return {ListNode}
  */
-var reverseKGroup = function (head, k) {
+ var reverseKGroup = function (head, k) {
+  if (head === null || head.next === null || k === 1) return head
+  let count = 1 // k个一组
+  // 会反转第一个节点，所以需要插入一个虚拟的头节点
+  let vHead = {}
+  // vHead.next = head
+  let reverseLast = null // 反转后的尾部
+  let fast = head        // 快指针
+  let slow = head        // 慢指针
+
+  function reverse (head, n) {
+    let next = null
+    let prev = null
+    const noteLast = head // 反转之后的最后一个节点
+    while (n > 0) {
+      next = head.next
+      head.next = prev
+      prev = head
+      head = next
+      n--
+    }
+    return {
+      first: prev,
+      last: noteLast
+    }
+  }
+  
+  while (fast) {
+    if (count !== k) {
+      fast = fast.next
+      count++
+      continue
+    }
+    fast = fast.next
+    // count 等于 k
+    const { first, last } = reverse(slow, k)
+    if (!vHead.next) {
+      vHead.next = first
+    } else {
+      reverseLast.next = first
+    }
+    reverseLast = last
+    slow = fast
+    count = 1
+  }
+  reverseLast.next = slow
+  return vHead.next
 };
+
+
+/*********测试用例 */
+// [1,2,3,4,5] 2
